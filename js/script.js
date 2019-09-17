c409384883fc8fb98aa1c24e2964c6563d2428d3
Alex Hoyau <a.hoyau@silexlabs.org>
2019-09-17 16:42:19 -0300

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 var HOME_PAGE_NAME = '72157648380751487'
         function flickIt(jQuerySelector, displayType, photosetId, opt_cbk){
-            jQuerySelector.html('<p class="flickr-msg">Chargement</p>');
+            //jQuerySelector.html('<p class="flickr-msg">Chargement</p>');
             console.log('flickit xxxxxxxxx', jQuerySelector, displayType)
             jQuerySelector.flickr({
                 api_key: "7b5f9448ae4547ff02d6ce3db22e482d",
